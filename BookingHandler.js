4e5e3f2bd6f05f70037f453c44cac4025700b556
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-11-25 10:49:15 +0100

    Clarified constants and doc, seperated message listener from the subscriber, started error handler in subscriber

--- BookingHandler.js
+++ BookingHandler.js
@@ -1,13 +1,18 @@
-/**
- * Constants that define if the connection via MQTT happens locally or remotely
- * @type {string}
- */
+/** Required libraries */
+const mqtt = require('mqtt');
+
+/** Different MQTT servers */
 const LOCALHOST = '' //TODO: fill with the local mqtt address
 const HOST = 'mqtt://test.mosquitto.org' //mosquitto test server address
 
-const mqtt = require('mqtt');
+/** Subscribed topics for MQTT */
+const subscribeBookingTopic = '/Team5/Dentistimo/Book'
+
+/** Published topics for MQTT */
+const publishStatusTopic = '/Team5/Dentistimo/BookingStatus'
+
 /**
- * Connects to the address defined in the constants above
+ * Connects to the servers defined in the constants above
  * @type {MqttClient}
  */
 const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
@@ -20,14 +25,18 @@ const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHO
  */
 client.on('connect', function() {
     console.log("Connected to Mqtt broker successfully" )
-    client.subscribe('/Team5/Dentistimo/Book', function (err) {
+    client.subscribe(subscribeBookingTopic, function (err) {
         if (!err) {
-            client.on('message', function (topic, message) {
-                const newBooking = JSON.parse(message.toString());
-                //TODO: post the new booking into the DB
-                //TODO: Probably adapt the message sent via MQTT or the topics to adapt if successful or not
-                client.publish('/Team5/Dentistimo/BookingStatus', newBooking.toString(), {qos:0} ) //TODO: This has to be changed into the response from the DB eventually
-            } )
+           console.log("Subscribed to " + subscribeBookingTopic + " successfully")
+        }else{
+            console.log(err.message);
         }
     })
 })
+
+client.on('message', function (topic, message) {
+    const newBooking = JSON.parse(message.toString());
+    //TODO: post the new booking into the DB
+    //TODO: Probably adapt the message sent via MQTT or the topics to adapt if successful or not
+    client.publish(publishStatusTopic, newBooking.stringify, {qos:0} ) //TODO: This has to be changed into the response from the DB eventually, QOS needs to be defined
+} )
