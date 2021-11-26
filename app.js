7573b7d44087a0d8020a4284cf1bc7300f46b4e4
astridbe <astridbe@student.chalmers.se>
2021-11-26 14:55:41 +0100

    establish database connection
    using mongoose and .env file

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
+
+const Booking = mongoose.model('Booking', { 
+  name: String,
+  userID: String,
+  date: String,
+  startTime: Number,
+  endTime: Number
+});
+
+const newBooking = new Booking({ 
+  dentistID: '1234',
+  userID: '1',
+  date: '2021-11-25',
+  startTime: 1100,
+  endTime: 1130
+});
+newBooking.save().then(() => console.log('saved booking'));
