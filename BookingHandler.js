b738e1afb365d1edf22788ae687aee08e34b2e1e
astridbe <astridbe@student.chalmers.se>
2021-11-29 09:47:50 +0100

    create models folder

--- BookingHandler.js
+++ BookingHandler.js
@@ -3,21 +3,15 @@ const mongoose = require("mongoose");
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
 
-const Booking = mongoose.model('Booking', { 
-  name: String,
-  userID: String,
-  requestID: String,
-  date: String,
-  startTime: String,
-  endTime: String
-});
+const Booking = require('./models/booking.js')
 
 const newBooking = new Booking({ 
   dentistID: '1234',
   userID: '1',
   requestID: '1',
   date: '2021-11-25',
-  startTime: '1100',
-  endTime: '1130'
+  startTime: '11:00',
+  endTime: '11:30'
 });
+
 newBooking.save().then(() => console.log('saved booking'));
