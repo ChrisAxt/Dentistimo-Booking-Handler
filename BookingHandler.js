14524b46bfa3b08c93b63906e926cb21f7983d63
astridbe <astridbe@student.chalmers.se>
2021-11-29 09:38:14 +0100

    update booking model

--- BookingHandler.js
+++ BookingHandler.js
@@ -6,16 +6,18 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@de
 const Booking = mongoose.model('Booking', { 
   name: String,
   userID: String,
+  requestID: String,
   date: String,
-  startTime: Number,
-  endTime: Number
+  startTime: String,
+  endTime: String
 });
 
 const newBooking = new Booking({ 
   dentistID: '1234',
   userID: '1',
+  requestID: '1',
   date: '2021-11-25',
-  startTime: 1100,
-  endTime: 1130
+  startTime: '1100',
+  endTime: '1130'
 });
 newBooking.save().then(() => console.log('saved booking'));
