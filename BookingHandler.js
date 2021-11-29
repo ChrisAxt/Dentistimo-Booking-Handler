e3d5e6bc6cc24c66f7205c3de3274896b59ca5f0
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-11-29 16:10:34 +0100

    Implement saving to database based on mocked data. Redefined QOS. Defining next steps as TODOS - Done with @astridbe

--- BookingHandler.js
+++ BookingHandler.js
@@ -12,12 +12,14 @@ const HOST = 'mqtt://test.mosquitto.org' //mosquitto test server address
 
 /** Connect to Database */
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
-
+//TODO: .env file with credential for this component -> user profile for component
 /** Subscribed topics for MQTT */
 const subscribeBookingTopic = '/Team5/Dentistimo/Book'
 
 /** Published topics for MQTT */
 const publishStatusTopic = '/Team5/Dentistimo/BookingStatus'
+
+//TODO: define options for mqtt connection (client ID, etc)
 
 /**
  * Connects to the servers defined in the constants above
@@ -25,15 +27,15 @@ const publishStatusTopic = '/Team5/Dentistimo/BookingStatus'
  */
 const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
 
-/** Mocked Data */
-const newBooking = new Booking({
+/** Mocked Data for testing */
+const testBooking = {
     dentistID: '1234',
     userID: '1',
     requestID: '1',
     date: '2021-11-25',
     startTime: '11:00',
     endTime: '11:30'
-});
+};
 
 /**
  * Subscribes to the topic
@@ -45,6 +47,7 @@ client.on('connect', function() {
     client.subscribe(subscribeBookingTopic, function (err) {
         if (!err) {
            console.log("Subscribed to " + subscribeBookingTopic + " successfully")
+           // client.publish(subscribeBookingTopic, JSON.stringify(testBooking), {qos:0}) // uncomment for testing using mocked data
         }else{
             console.log(err.message);
         }
@@ -52,8 +55,14 @@ client.on('connect', function() {
 })
 
 client.on('message', function (topic, message) {
-    const newBooking = JSON.parse(message.toString());
-    newBooking.save().then(() => console.log('saved booking'));
-    //TODO: Probably adapt the message sent via MQTT or the topics to adapt if successful or not
-    client.publish(publishStatusTopic, newBooking.stringify, {qos:0} ) //TODO: This has to be changed into the response from the DB eventually, QOS needs to be defined
+    let newBooking = new Booking(JSON.parse(message.toString()));
+    newBooking.save(function (err, newBooking) {
+        if (err) {
+            console.log(err.message)
+            client.publish(publishStatusTopic, err.message, {qos:1} )
+        }else{
+            client.publish(publishStatusTopic, JSON.stringify(newBooking), {qos:1} )
+            console.log('Booking saved')
+        }
+    });
 } )
