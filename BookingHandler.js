0627edd43cdb564ccfe1bc796686cf9e31b71c88
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-11-30 11:05:15 +0100

    Refactored the code to make it clearer and using helper methods for better reusability. Reviewed comments

--- BookingHandler.js
+++ BookingHandler.js
@@ -3,30 +3,6 @@ require('dotenv').config()
 const mqtt = require('mqtt');
 const mongoose = require("mongoose");
 
-/** Database Models */
-const Booking = require('./models/booking.js')
-
-/** Different MQTT servers */
-const LOCALHOST = '' //TODO: fill with the local mqtt address
-const HOST = 'mqtt://test.mosquitto.org' //mosquitto test server address
-
-/** Connect to Database */
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
-//TODO: .env file with credential for this component -> user profile for component
-/** Subscribed topics for MQTT */
-const subscribeBookingTopic = '/Team5/Dentistimo/Book'
-
-/** Published topics for MQTT */
-const publishStatusTopic = '/Team5/Dentistimo/BookingStatus'
-
-//TODO: define options for mqtt connection (client ID, etc)
-
-/**
- * Connects to the servers defined in the constants above
- * @type {MqttClient}
- */
-const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
-
 /** Mocked Data for testing */
 const testBooking = {
     dentistID: '1234',
@@ -37,32 +13,100 @@ const testBooking = {
     endTime: '11:30'
 };
 
+/** Database Models */
+const Booking = require('./models/booking.js')
+
+/** Different MQTT servers */
+const LOCALHOST = '' //TODO: fill with the local mqtt address
+const HOST = 'mqtt://test.mosquitto.org' //mosquitto test server address
+
+/** Subscribed topics for MQTT */
+const createBookingTopic = '/Team5/Dentistimo/Booking/Create'
+const deleteBookingTopic = '/Team5/Dentistimo/Booking/Delete'
+
+/** Published topics for MQTT */
+const statusTopic = '/Team5/Dentistimo/BookingStatus'
+
+//TODO: define options for mqtt connection (client ID, etc)
+
+/** Connect to Database */
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@dentistimo0.vd9sq.mongodb.net/Dentistimo`);
+
 /**
- * Subscribes to the topic
- * Posts a new booking with the info received in the message
- * Publish the confirmation via MQTT
+ * Connects to the servers defined in the constants above
+ * @type {MqttClient}
+ */
+const client = mqtt.connect(HOST) //Change the parameter between HOST or LOCALHOST if you want to connect to the mosquitto test broker or a local broker. For local, mosquitto needs to be installed and running
+
+/**
+ * Subscribes to the needed topic(s)
+ * Manage tests for each topic.
  */
 client.on('connect', function() {
     console.log("Connected to Mqtt broker successfully" )
-    client.subscribe(subscribeBookingTopic, function (err) {
+    subscribeToTopic(createBookingTopic, false, testBooking);
+})
+
+/**
+ * Helper method that subscribe to a certain topic and react to the subscription.
+ * Publish also to the subscribed topic for testing purposes if test boolean is true.
+ * @param topic of type String
+ * @param testIsActive of type boolean
+ * @param mockedData of type JSON object
+ */
+function subscribeToTopic(topic, testIsActive, mockedData){
+    client.subscribe(topic, function (err) {
         if (!err) {
-           console.log("Subscribed to " + subscribeBookingTopic + " successfully")
-           // client.publish(subscribeBookingTopic, JSON.stringify(testBooking), {qos:0}) // uncomment for testing using mocked data
+            console.log("Subscribed to " + topic + " successfully")
+            if(testIsActive){
+                testTopic(topic, mockedData);
+            }
         }else{
             console.log(err.message);
         }
     })
+}
+
+/**
+ * Helper method for testing a topic.
+ * Publish to a given topic a mocked json object.
+ * @param isActive
+ * @param mockedData
+ */
+function testTopic(topic, mockedData) {
+    client.publish(topic, JSON.stringify(mockedData), {qos:0})
+}
+
+/**
+ * Listen to topic(s) and call the right method based on the topic.
+ */
+client.on('message', function (topic, message) {
+
+    switch (topic){
+        case createBookingTopic:
+            saveBooking(message);
+            break;
+        case deleteBookingTopic:
+            //TODO: Insert the delete function here when coded
+            break;
+        default:
+            break;
+    }
 })
 
-client.on('message', function (topic, message) {
-    let newBooking = new Booking(JSON.parse(message.toString()));
+/**
+ * Helper method that save a new booking into the database
+ * @param mqttMessage of type Buffer
+ */
+function saveBooking(mqttMessage){
+    let newBooking = new Booking(JSON.parse(mqttMessage.toString()));
     newBooking.save(function (err, newBooking) {
         if (err) {
             console.log(err.message)
-            client.publish(publishStatusTopic, err.message, {qos:1} )
+            client.publish(statusTopic, err.message, {qos:1} )
         }else{
-            client.publish(publishStatusTopic, JSON.stringify(newBooking), {qos:1} )
+            client.publish(statusTopic, JSON.stringify(newBooking), {qos:1} )
             console.log('Booking saved')
         }
     });
-} )
+}
